18294e7667d47a5376c2234c50bb6ee8a993b121
Aditya <maruthikasyapaditya@gmail.com>
2018-09-23 19:26:19 +0530

    Before Stable

---
+++ src/com/raylock/entity/Entity.java
@@ -0,0 +1,30 @@
+package com.raylock.entity;
+
+import com.raylock.graphics.Screen;
+import com.raylock.level.Level;
+import java.util.Random;
+
+public class Entity {
+
+    public int x, y;
+    private boolean removed = false;
+    protected Level level;
+    protected final Random random = new Random();
+
+    public void update() {
+
+    }
+
+    public void render(Screen screen) {
+
+    }
+
+    public void remove() {
+        //Remove from level
+        removed = true;
+    }
+
+    public boolean isRemoved() {
+        return removed;
+    }
+}
