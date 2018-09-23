bb6f1f789140697e907d51cd592ec6001a23c5a2
Aditya <maruthikasyapaditya@gmail.com>
2018-09-23 19:01:24 +0530

    Before Stable

---
+++ src/com/raylock/tile/voidTile.java
@@ -0,0 +1,11 @@
+package com.raylock.tile;
+
+import com.raylock.graphics.Sprite;
+
+public class voidTile extends Tile {
+
+    public voidTile(Sprite voidSprite) {
+        super(voidSprite);
+    }
+
+}
