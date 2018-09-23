7dd6c3e81d0c4671f11b159ab1cef70bc23b8358
Aditya <maruthikasyapaditya@gmail.com>
2018-09-23 16:27:32 +0530

    Before Stable

--- src/com/raylock/graphics/Screen.java
+++ src/com/raylock/graphics/Screen.java
@@ -1,14 +1,26 @@
 package com.raylock.graphics;
 
+import com.raylock.tile.Tile;
+import java.util.Random;
+
 public class Screen {
 
     private int width, height;
     public int pixels[];
+    public final int MAP_SIZE = 64;
+    public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+    public int tiles[] = new int[64 * 64];
+
+    private Random random = new Random();
 
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+
+        for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
+            tiles[i] = random.nextInt(0xffffff);
+        }
     }
 
     public void clear() {
@@ -17,10 +29,30 @@ public class Screen {
         }
     }
 
-    public void render() {
+    public void render(int xOffset, int yOffset) {
         for (int y = 0; y < height; y++) {
+            int yp = y + yOffset;
+            if (yp < 0 || yp >= height) {
+                continue;
+            }
             for (int x = 0; x < width; x++) {
-                pixels[x + y * width] = 0xffffff;
+                int xp = x + xOffset;
+                if (xp < 0 || xp >= width) {
+                    continue;
+                }
+                pixels[xp + yp * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
+            }
+        }
+    }
+
+    public void renderTile(int xp, int yp, Tile tile) {
+        for (int y = 0; y < tile.sprite.SIZE; y++) {
+            int ya = y + yp;
+            for (int x = 0; x < tile.sprite.SIZE; x++) {
+                int xa = x + xp;
+                if (xa < 0 || xa >= width || ya < 0 || ya >= height) {
+                    break;
+                }
             }
         }
     }
