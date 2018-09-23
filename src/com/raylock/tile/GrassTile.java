ab12804ad460d28f5ac6638d3d4718bd29c62ba5
Aditya <maruthikasyapaditya@gmail.com>
2018-09-23 16:28:07 +0530

    Before Stable

---
+++ src/com/raylock/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package com.raylock.tile;
+
+import com.raylock.graphics.Screen;
+import com.raylock.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+    public GrassTile(Sprite sprite) {
+        super(sprite);
+    }
+
+    public void render(int x, int y, Screen screen) {
+
+    }
+
+}
