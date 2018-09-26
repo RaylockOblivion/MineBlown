44ec485d6c29b96d01961de8e9269807b42b7573
Aditya <maruthikasyapaditya@gmail.com>
2018-09-26 20:54:55 +0530

    stable

---
+++ src/com/raylock/entity/mob/Player.java
@@ -0,0 +1,50 @@
+package com.raylock.entity.mob;
+
+import com.raylock.graphics.Screen;
+import com.raylock.graphics.Sprite;
+import com.raylock.input.Keyboard;
+
+public class Player extends Mob {
+
+    private Keyboard input;
+
+    public Player(Keyboard input) {
+        this.input = input;
+    }
+
+    public Player(int x, int y, Keyboard input) {
+        this.x = x;
+        this.y = y;
+        this.input = input;
+    }
+
+    public void update() {
+        int xa = 0, ya = 0;
+
+        if (input.up) {
+            ya--;
+        }
+        if (input.down) {
+            ya++;
+        }
+        if (input.left) {
+            xa--;
+        }
+        if (input.right) {
+            xa++;
+        }
+
+        if (xa != 0 || ya != 0) {
+            move(xa, ya);
+        }
+    }
+
+    public void render(Screen screen) {
+        int xx = x - 16;
+        int yy = y - 16;
+        screen.renderPlayer(xx, yy, Sprite.player0);
+        screen.renderPlayer(xx + 16, yy, Sprite.player1);
+        screen.renderPlayer(xx, yy + 16, Sprite.player2);
+        screen.renderPlayer(xx + 16, yy + 16, Sprite.player3);
+    }
+}
