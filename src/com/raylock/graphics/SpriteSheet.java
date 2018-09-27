a475c77f1716930674fc6491ffad5976cf7445b8
Aditya <maruthikasyapaditya@gmail.com>
2018-09-27 12:33:38 +0530

    custom level

--- src/com/raylock/graphics/SpriteSheet.java
+++ src/com/raylock/graphics/SpriteSheet.java
@@ -10,7 +10,7 @@ public class SpriteSheet {
     public final int SIZE;
     public int pixels[];
 
-    public static SpriteSheet tiles = new SpriteSheet("/textures/test.png", 256);
+    public static SpriteSheet tiles = new SpriteSheet("/textures/spritesheet.png", 256);
 
     public SpriteSheet(String path, int size) {
         this.path = path;
